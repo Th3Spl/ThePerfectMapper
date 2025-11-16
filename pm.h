6dad849b6a942ca80d927d70bdddc3bd59f50ad1
Th3Spl <stepro2006@gmail.com>
2025-11-16 01:09:37 +0100

    added fixup for C++

--- pm.h
+++ pm.h
@@ -2,28 +2,48 @@
 #pragma once
 
 
+/* C++ helper ( otherwise name will be decorated ) */
+#if defined(__cplusplus)
+#define cpp_fixup_s extern "C" {
+#define cpp_fixup_e }
+#else
+#define cpp_fixup_s
+#define cpp_fixup_e
+#endif
+
+
 /* includes a simple iniitalization check */
 #define pm_iat_check( check_name ) \
-	__pragma(comment(linker, "/EXPORT:#PM#@check" "=" #check_name ",DATA")) \
-	char check_name;
+	cpp_fixup_s \
+	__pragma( comment( linker, "/EXPORT:#PM#@check" "=" #check_name ",DATA" ) ) \
+	char check_name; \
+	cpp_fixup_e
 
 /* Adds a PDB IAT in the EAT for a function ( needs an existing typedef ) */
 #define pm_iat(func_name, _typedef, _src_module, _src_name) \
+	cpp_fixup_s \
     __pragma(comment(linker, "/EXPORT:#PM#" #func_name "#" _src_module "#" _src_name "=" #func_name ",DATA")) \
-	_typedef func_name;
+	_typedef func_name; \
+	cpp_fixup_e
 
 /* Adds a PDB IAT in the EAT for a function ( does not need an existing typedef ) */
 #define pm_iat_ex( func_name, ret, call_conv, type_name, args, _src_module, _src_name) \
+	cpp_fixup_s \
 	__pragma(comment(linker, "/EXPORT:#PM#" #func_name "#" _src_module "#" _src_name "=" #func_name ",DATA")) \
 	typedef ret( call_conv* type_name) args; \
-	type_name func_name;
+	type_name func_name; \
+	cpp_fixup_e
 
 /* Adds a PDB IAT in the EAT for an offset within a structure (ex: _EPROCESS->DirectoryTableBase ) */
 #define pm_iat_offset( _name, _src_module, _src_struct, _src_comp_name ) \
+	cpp_fixup_s \
 	__pragma( comment( linker, "/EXPORT:#PM#" #_name "#@offset#" _src_module "#" _src_struct "#" _src_comp_name "=" #_name ",DATA" )) \
-	unsigned long long _name;
-
+	unsigned long long _name; \
+	cpp_fixup_e
+ 
 /* Adds a PDB IAT in the EAT for an offset within a structure with a specified type for the offset */
 #define pm_iat_offset2( _name, _type, _src_module, _src_struct, _src_comp_name ) \
+	cpp_fixup_s \
 	__pragma( comment( linker, "/EXPORT:#PM#" #_name "#@offset#" _src_module "#" _src_struct "#" _src_comp_name "=" #_name ",DATA" )) \
-	_type _name;
+	_type _name; \
+	cpp_fixup_e 
